eb474cfdf59a2f109fddf42a4f6f2f663d28a493
Anannya Dey <anannyaneha@gmail.com>
2025-03-15 19:49:58 +0530

    lk

--- py.js
+++ py.js
@@ -1,5 +1,3 @@
-let n=5;
-for(let i=0;i<n;i++){
-   console.log("nehaa");
-}
-console.log("the process id is:",ProcessingInstruction.arguments);
+const math=require("./u");
+console.log(math.sum(2,2));
+console.log(math.pi);
---
+++ u.js
@@ -0,0 +1,3 @@
+Module.exports.sum=(a,b)=>a+b;
+Module.exports.mul=(a,b)=>a*b;
+Module.exports.p
